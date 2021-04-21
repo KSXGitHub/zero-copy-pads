ac84cc4989c2832e76ee6fba19720df8e318b086
khai96_ <hvksmr1996@gmail.com>
2021-04-21 21:44:35 +0700

    refactor: Rearrange variants of Alignment

--- src/alignment.rs
+++ src/alignment.rs
@@ -1,24 +1,6 @@
 /// Where the place the pad blocks.
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 pub enum Alignment {
-    /// Pad to the left, content to the right.
-    ///
-    /// **Example:**
-    ///
-    /// ```
-    /// # use pretty_assertions::assert_eq;
-    /// use padded_column::{Alignment::Right, PaddedValue, ForbidExcess};
-    /// let padded_value = PaddedValue {
-    ///     pad_direction: Right,
-    ///     value: "abcdef",
-    ///     pad_block: '-',
-    ///     total_width: 9,
-    ///     handle_excess: ForbidExcess,
-    /// };
-    /// assert_eq!(padded_value.to_string(), "---abcdef");
-    /// ```
-    Right,
-
     /// Pad to the right, content to the left.
     ///
     /// **Example:**
@@ -36,4 +18,22 @@ pub enum Alignment {
     /// assert_eq!(padded_value.to_string(), "abcdef---");
     /// ```
     Left,
+
+    /// Pad to the left, content to the right.
+    ///
+    /// **Example:**
+    ///
+    /// ```
+    /// # use pretty_assertions::assert_eq;
+    /// use padded_column::{Alignment::Right, PaddedValue, ForbidExcess};
+    /// let padded_value = PaddedValue {
+    ///     pad_direction: Right,
+    ///     value: "abcdef",
+    ///     pad_block: '-',
+    ///     total_width: 9,
+    ///     handle_excess: ForbidExcess,
+    /// };
+    /// assert_eq!(padded_value.to_string(), "---abcdef");
+    /// ```
+    Right,
 }
