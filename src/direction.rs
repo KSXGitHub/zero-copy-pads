0c675de5502658d766e1dadce762351bf1d2ec53
khai96_ <hvksmr1996@gmail.com>
2021-04-21 17:27:16 +0700

    doc+test: Add code examples to variants of PadDirection

--- src/direction.rs
+++ src/direction.rs
@@ -2,7 +2,38 @@
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 pub enum PadDirection {
     /// Pad to the left, content to the right.
+    ///
+    /// **Example:**
+    ///
+    /// ```
+    /// # use pretty_assertions::assert_eq;
+    /// use padded_column::{PadDirection::Left, PaddedItem, ForbidExcess};
+    /// let padded_item = PaddedItem {
+    ///     pad_direction: Left,
+    ///     value: "abcdef",
+    ///     pad_block: '-',
+    ///     total_width: 9,
+    ///     handle_excess: ForbidExcess,
+    /// };
+    /// assert_eq!(padded_item.to_string(), "---abcdef");
+    /// ```
     Left,
+
     /// Pad to the right, content to the left.
+    ///
+    /// **Example:**
+    ///
+    /// ```
+    /// # use pretty_assertions::assert_eq;
+    /// use padded_column::{PadDirection::Right, PaddedItem, ForbidExcess};
+    /// let padded_item = PaddedItem {
+    ///     pad_direction: Right,
+    ///     value: "abcdef",
+    ///     pad_block: '-',
+    ///     total_width: 9,
+    ///     handle_excess: ForbidExcess,
+    /// };
+    /// assert_eq!(padded_item.to_string(), "abcdef---");
+    /// ```
     Right,
 }
